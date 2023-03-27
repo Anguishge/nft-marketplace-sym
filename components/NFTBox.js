5bd11e16197290c6efeea56c5badaf5f4a1b94f6
Anguishge <942165800@qq.com>
2023-03-28 01:30:39 +0800

    add 1st comment

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -78,6 +78,7 @@ export default function NFTBox({
         }
         //get tokenURI
         //using the image tag from the tokenURI, get the image
+        //add comment
     }
 
     useEffect(() => {
