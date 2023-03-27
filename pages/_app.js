91e309f45da4ee8b9cb4dd02a40ee0e4a05de67c
Anguishge <942165800@qq.com>
2023-03-28 01:27:34 +0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,33 @@
-import '@/styles/globals.css'
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import { NotificationProvider } from "web3uikit"
+import Head from "next/head"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://api.studio.thegraph.com/query/43870/nft-marketplace/v0.0.1",
+})
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <ApolloProvider client={client}>
+                    <NotificationProvider>
+                        <Header />
+                        <Component {...pageProps} />
+                    </NotificationProvider>
+                </ApolloProvider>
+            </MoralisProvider>
+        </div>
+    )
 }
+
+export default MyApp
